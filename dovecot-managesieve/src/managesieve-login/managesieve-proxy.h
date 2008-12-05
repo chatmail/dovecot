ba969da0b4f3521bb38f3cd413497a229bb19da2
Joel Johnson <mrjoel@lixil.net>
2008-12-05 03:58:05 +0000

    remove managesieve 0.10.3

--- dovecot-managesieve/src/managesieve-login/managesieve-proxy.h
+++
@@ -1,9 +0,0 @@
-#ifndef __MANAGESIEVE_PROXY_H
-#define __MANAGESIEVE_PROXY_H
-
-#include "login-proxy.h"
-
-int managesieve_proxy_new(struct managesieve_client *client, const char *host,
-		   unsigned int port, const char *user, const char *password);
-
-#endif
