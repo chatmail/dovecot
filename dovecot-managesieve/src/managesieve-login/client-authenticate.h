253bc1e36c6842ed8274a333a0415f3a473051ae
Fabio Tranchitella <kobold@debian.org>
2009-02-27 15:59:16 +0000

    * debian/dovecot-common.init: check if /etc/inetd.conf exists. (Closes: #509259) * debian/dovecot-common.postinst: migration hook for default_mail_env =>   mail_location. (Closes: #517073)

--- dovecot-managesieve/src/managesieve-login/client-authenticate.h
+++
@@ -1,9 +0,0 @@
-#ifndef __CLIENT_AUTHENTICATE_H
-#define __CLIENT_AUTHENTICATE_H
-
-const char *client_authenticate_get_capabilities(bool secured);
-
-int cmd_login(struct managesieve_client *client, struct managesieve_arg *args);
-int cmd_authenticate(struct managesieve_client *client, struct managesieve_arg *args);
-
-#endif
--- dovecot-sieve/src/libconfig.h
+++
@@ -1,8 +0,0 @@
-#ifndef __LIBCONFIG_H
-#define __LIBCONFIG_H
-
-#define IMAPOPT_RFC3028_STRICT 1
-
-#define config_getswitch(n) 1
-
-#endif
