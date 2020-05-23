a65948da1b814dd7185e9c72752c2dc3a04e10cf
Christian Göttsche <cgzones@googlemail.com>
2020-05-23 20:07:46 +0200

    d/source_dovecot.py: switch to python3 shebang
    Also make pylint more happy

--- debian/source_dovecot.py
+++ debian/source_dovecot.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 '''Dovecot Apport interface
 
@@ -15,25 +15,24 @@ the full text of the license.
 from apport.hookutils import *
 
 def add_info(report, ui):
-   response = ui.yesno("The output of dovecot -n may help developers diagnose your bug more quickly, however, it may contain sensitive information. Do you want to include it in your bug report?")
+    response = ui.yesno("The output of dovecot -n may help developers diagnose your bug more quickly, however, it may contain sensitive information. Do you want to include it in your bug report?")
 
-   if response == None: #user canceled
-       raise StopIteration
+    if response is None: #user canceled
+        raise StopIteration
 
-   elif response == True:
-       report['DovecotConf'] = root_command_output(['/usr/sbin/dovecot', '-n'])
+    elif response:
+        report['DovecotConf'] = root_command_output(['/usr/sbin/dovecot', '-n'])
 
 
-   elif response == False:
-       ui.information("The contents of dovecot -n will NOT be includeded in the bug report")
+    elif not response:
+        ui.information("The contents of dovecot -n will NOT be includeded in the bug report")
 
-   packages=['dovecot-common', 'dovecot-core', 'dovecot-dev', 'dovecot-pop3d', 'dovecot-imapd',  'mail-stack-delivery', 'dovecot-postfix']
-   versions = ''
-   for package in packages:
-       try:
-           version  = package.get_version(package)
-       except:
-           version = 'N/A'
-       versions += '%s %s\n' %(package, version)
-   report['DovecotInstalledVersions'] = versions
-
+    packages = ['dovecot-common', 'dovecot-core', 'dovecot-dev', 'dovecot-pop3d', 'dovecot-imapd', 'mail-stack-delivery', 'dovecot-postfix']
+    versions = ''
+    for package in packages:
+        try:
+            version = package.get_version(package)
+        except:
+            version = 'N/A'
+        versions += '%s %s\n' %(package, version)
+    report['DovecotInstalledVersions'] = versions
